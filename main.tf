52d760f6a9dd7a254f15d1808994eb9d5e9dbafd
ibra_gaming <vibrahim1217@gmail.com>
2025-05-26 21:59:31 -0700

    update ignore file

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.0.0-beta2"
     }
   }
-    required_version = ">= 1.2.0"
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region  = "us-west-2"
-  shared_credentials_files = [ 
+  region = "us-west-2"
+  shared_credentials_files = [
     "${path.module}/.aws/credentials"
   ]
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  ami           = "ami-075686beab831bb7f"
+  instance_type = "t2.medium"
 
   tags = {
     Name = "ExampleAppServerInstance"
