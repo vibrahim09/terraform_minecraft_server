0cabbc3485306c6457a30caca32e6e7eb5f17c1a
Ibrahim Velasquez <vibrahim09@gmail.com>
2025-05-27 21:42:02 -0700

    add security group

--- main.tf
+++ main.tf
@@ -15,17 +15,43 @@ provider "aws" {
   ]
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "minecraft_project_2" {
   ami           = "ami-075686beab831bb7f"
   instance_type = "t2.medium"
   key_name      = "minecraft_project_2" # Replace with your key pair name
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
-  security_groups = [aws_security_group.app_sg.name]
+
   
   associate_public_ip_address = true
 
 
   tags = {
     Name = var.instance_name
+  }
+}
+
+resource "aws_security_group" "minecraft_project_2_sg" {
+  name        = "minecraft_project_2_sg"
+  description = "Security group for the Minecraft server"
+
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0/0"] # Allow all IPs to connect to the Minecraft server
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0/0"] # Allow SSH access from anywhere (not recommended for production)
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allow all outbound traffic
+    cidr_blocks = ["0.0.0/0"]
+  }
+  tags = {
+    Name = "minecraft_project_2_sg"
   }
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "instance_name" {
   type        = string
   default     = "Minecraft Server"
 }
+
+variable "vpc_id" {
+  description = "VPC ID where the EC2 instance will be launched"
+  type        = string
+  default     = "vpc-12345678" # Replace with your VPC ID
+}
